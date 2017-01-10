820c714a70cc0d701c1d8eb8e4465b5e0a7f9594
AlecDakin <AlecDakin@users.noreply.github.com>
2017-01-10 16:27:34 +0000

    Lots of changes, mainly uses random Useragent

--- website.sh
+++ website.sh
@@ -1,68 +1,86 @@
 #!/bin/bash
 ## Notes to go here...
 
-run=5
-batch=10
-pause=2
+RUN=5
+BATCH=10
+PAUSE=2
 
 internet_wait ()
 {
-	PL=$(ping -c 1 -q  8.8.8.8 | grep -oP '\d+(?=% packet loss)')
-	while [ $PL == 100 ]; do
+	local PL=$(ping -c 1 -q  8.8.8.8 | grep -oP '\d+(?=% packet loss)')
+	while [ $PL -eq 100 ]; do
 		echo "Offline"
-		count=$( ps -A | grep -o wget | wc -l )
-		if [ $count -ge 1 ]; then
+		local COUNT=$( ps -A | grep -o wget | wc -l )
+		if [ $COUNT -ge 1 ]; then
 			pkill wget
 		fi
-		## sleep $pause
 		PL=$(ping -c 5 -q  8.8.8.8 | grep -oP '\d+(?=% packet loss)')
 	done
 }
 
 wget_wait ()
 {
-	count=$( ps -A | grep -o wget | wc -l )
-	while [ $count -ge $run ]; do
-		sleep $pause
-		count=$( ps -A | grep -o wget | wc -l )
-		#echo $count
+	local COUNT=$( ps -A | grep -o wget | wc -l )
+	while [ $COUNT -ge $RUN ]; do
+		sleep $PAUSE
+		COUNT=$( ps -A | grep -o wget | wc -l )
 	done
 }
 
+get_agent ()
+{
+	if [ -s agents.txt ]; then
+		UAGENT="Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; MATBJS; rv:11.0) like Gecko"
+	else
+		UAGENT=$( shuf -n 1 agents.txt ) 
+	fi
+}
+
+get_site ()
+{
+
+	local ADDRESS=$1
+	local FILENAME="${ADDRESS##*/}"
+	
+	if [ -f $FILENAME ]; then
+		wget -U "$UAGENT" -N "$ADDRESS"
+	else
+		wget -U "$UAGENT" -S "$ADDRESS"
+	fi
+}
+
+
 while :
 do 
+	get_agent
 	internet_wait
 	
-	if [ -f top-1m.csv.zip ]; then
-		wget -N https://s3.amazonaws.com/alexa-static/top-1m.csv.zip 
-	else
-		wget -S https://s3.amazonaws.com/alexa-static/top-1m.csv.zip 
-	fi
-
-	if [ -f top-1million-sites.csv.zip ]; then
-		wget -N https://statvoo.com/dl/top-1million-sites.csv.zip
-	else
-		wget -S https://statvoo.com/dl/top-1million-sites.csv.zip
-	fi
-
+	get_site "http://techpatterns.com/downloads/firefox/useragentswitcher.xml"
+	
+	grep -oP 'useragent="\K[^"]*' useragentswitcher.xml > agents.txt
+	
+	get_site "https://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
+	get_site "https://statvoo.com/dl/top-1million-sites.csv.zip"
+	
 	gzip -dc  top-1m.csv.zip | pv -s 22000000 | sed 's/[^,]*,//' > /tmp/test1.txt
 	gzip -dc  top-1million-sites.csv.zip | pv -s 22000000 | sed 's/[^,]*,//' > /tmp/test2.txt
 
-	cat /tmp/test1.txt /tmp/test2.txt | pv | sort -uR > sites.txt
+	sort -fuR /tmp/test1.txt /tmp/test2.txt > sites.txt
 
 	rm /tmp/test1.txt
 	rm /tmp/test2.txt
 	
-	sites=0
-	time=$SECONDS
-	uagent="Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; MATBJS; rv:11.0) like Gecko"
-	while read p; do
-		sites=$((sites+1))
-		printf "$sites -> $p\n"
-        wget -U "$uagent" -T 8 -t 1 -4bq -O /dev/null --no-cookies --https-only "https://$p" > /dev/null
-		if [ $((sites % batch)) -eq 0 ] ; then
-			speed=$(( sites / ((SECONDS - time)+1)))
-			printf "$speed websites per second.\n"
+	SITES=0
+	TIME=$SECONDS
+	
+	while read LINE; do
+		((SITES++))
+		printf "$SITES -> $LINE\n"
+        wget -U "$UAGENT" -T 8 -t 1 -4bq -O /dev/null --no-cookies --https-only "https://$LINE" > /dev/null
+		if [ $((SITES % BATCH)) -eq 0 ] ; then
+			SPEED=$(( SITES / ((SECONDS - TIME)+1)))
+			printf "$SPEED websites per second.\n"
+			get_agent
 			internet_wait
 			wget_wait
 		fi
